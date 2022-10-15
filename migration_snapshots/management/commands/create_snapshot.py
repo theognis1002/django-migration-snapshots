a66727bbccc828d1b97bbf6b0842b0e4a4873705
theognis1002 <theognis1002@gmail.com>
2022-10-14 20:27:00 -0400

    Random update

--- migration_snapshots/management/commands/create_snapshot.py
+++ migration_snapshots/management/commands/create_snapshot.py
@@ -5,7 +5,13 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("--format", help="output format for digraph")
-        parser.add_argument("--caption", help="caption for digraph out")
+        parser.add_argument(
+            "--date",
+            help="end date for migration history output",
+            required=False,
+            default="",
+        )
+        # parser.add_argument("--caption", help="caption for digraph out")
 
     def handle(self, *args, **options):
         """
